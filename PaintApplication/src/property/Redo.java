79d66d290b0f2e2baa63f1b715f12db0d4c1af3b
Phạm Tuấn Hiên <46740749+phamtuanhien@users.noreply.github.com>
2020-05-10 22:01:11 +0700

    .

--- PaintApplication/src/property/Redo.java
+++ PaintApplication/src/property/Redo.java
@@ -15,7 +15,7 @@ import java.awt.image.WritableRaster;
 public class Redo {
     private int data[][];
     private final int size = 100;
-    private int topRedo, countRedo;
+    private int topRedo;
     private int[] w, h;
     private BufferedImage img;
     
@@ -24,32 +24,37 @@ public class Redo {
         w = new int[size];
         h = new int[size];
         topRedo = -1;
-        countRedo = 0;
     }
     public void push(BufferedImage buff_img) {
-        if (countRedo < 100) ++countRedo;
-        topRedo = (topRedo + 1) % 100;
+        topRedo++;
         w[topRedo] = buff_img.getWidth();
         h[topRedo] = buff_img.getHeight();
         WritableRaster raster = buff_img.getRaster();
         data[topRedo] = raster.getPixels(0, 0, w[topRedo], h[topRedo], data[topRedo]);
     }
     public BufferedImage pop() {
-        if (countRedo > 0 && data[topRedo] != null) {
+        if (data[topRedo] != null) {
             img = new BufferedImage(w[topRedo], h[topRedo] , BufferedImage.TYPE_INT_RGB);
             img.getRaster().setPixels(0, 0, w[topRedo], h[topRedo], data[topRedo]);
-            countRedo--;
             w[topRedo] = 0;
             h[topRedo] = 0;
             data[topRedo] = null;
-            topRedo = (topRedo - 1) % 100;
+            topRedo--;
             return img;
         }
         return null;
     }
     public boolean isEmpty() {
-        if (countRedo == 0) return true;
+        if (topRedo == -1) return true;
         return false;
+    }
+    public void clear() {
+        if (topRedo > -1) {
+            w[topRedo] = 0;
+            h[topRedo] = 0;
+            data[topRedo] = null;
+            topRedo--;
+        }
     }
     
 }
