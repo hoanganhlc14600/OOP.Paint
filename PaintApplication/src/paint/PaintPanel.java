4f73d6cd28385a740e83ac894237c6c2e44ee0e8
dai02012000 <ledai02012000@gmail.com>
2020-05-10 09:17:16 +0700

    Update PaintPanel.java

--- PaintApplication/src/paint/PaintPanel.java
+++ PaintApplication/src/paint/PaintPanel.java
@@ -306,7 +306,7 @@ public class PaintPanel extends javax.swing.JPanel implements MouseListener, Mou
                 break;
             case "ERASER":
                 eraser.setPoint(startPoint, startPoint);
-                eraser.setStrokeColor(colorChooser.getStrokeColor());
+                eraser.setStrokeColor(colorChooser.getFillColor());
                 eraser.setSize(10);
                 eraser.setStroke(stroke.getStroke());
                 eraser.draw(g2d);
